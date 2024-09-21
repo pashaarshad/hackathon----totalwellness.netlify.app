bca5cc200b6518706f8602ac40b7960cb89b8400
arshad786pasha <arshad785pasha65@gmail.com>
2024-09-21 19:31:07 +0530

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+let x = document.getElementById('menu');
+let menuBtn = document.querySelector('.menuBtn');
+let closeBtn = document.querySelector('.closeBtn');
+// x.style.display = 'none';
+
+function openMenu(){
+    
+    x.style.display = 'block';
+    menuBtn.style.display  = 'none';
+    closeBtn.style.display ='block';
+}
+function closeMenu(){
+    x.style.display ='none';
+    menuBtn.style.display = 'block';
+    closeBtn.style.display ='none';
+}
